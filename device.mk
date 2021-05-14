a95d6a3802862de882290016d6688cd574c02538
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    begonia: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf

--- device.mk
+++ device.mk
@@ -312,6 +312,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor
